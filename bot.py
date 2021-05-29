0571b18e4e14685ec2245c2981a2d67799af3877
Emre Boyacı <emre.boyaci@external.yapikredi.com.tr>
2021-05-29 21:33:44 +0300

    My first commit

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ def signup(update, context):
 
 def atm(update, context):
     try:
-        cur.execute("SELECT money FROM users WHERE userID = '%s'",
+        cur.execute("SELECT money FROM users WHERE userID = %s",
                     (str(update.message.from_user.id), ))
         print(cur.fetchone())
         update.message.reply_text("You have %s money", (cur.fetchone(),))
