e19302f4c91cb8ab2b07762d77abf08ec4e4d70f
Emre Boyacı <emre.boyaci@external.yapikredi.com.tr>
2021-05-29 21:40:09 +0300

    My first commit

--- bot.py
+++ bot.py
@@ -60,7 +60,7 @@ def atm(update, context):
         cur.execute("SELECT money FROM users WHERE userID = %s",
                     (str(update.message.from_user.id), ))
 
-        update.message.reply_text("You have" + cur.fetchone() + "money")
+        update.message.reply_text("You have" + str(cur.fetchone()[0]) + "money")
     except Exception as error:
         print(error)
 
