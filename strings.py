140b5feef7350ffafaf571d6ecf16ad1dd0213a1
Emre Boyacı <emre.boyaci@external.yapikredi.com.tr>
2021-05-31 00:49:17 +0300

    String refactoring

--- strings.py
+++ strings.py
@@ -1,5 +1,4 @@
-help = '''Hey 👋... \n\n /signup nick: enter your nick and sign up \n\n /atm: see your balance \n\n /send nick
-amount: send this nick that amount of buxx 💰 \n '''
+help = '''Hey 👋 \n\n /signup nick: enter your nick and sign up \n\n /atm: see your balance \n\n /send nick amount: send this nick that amount of buxx 💰 \n '''
 signup = '''Hi! Type /signup to sign up.'''
 user_created = '''Created user. Welcome to Paybot 🤑'''
 user_exists = '''User already exists ☹️'''
